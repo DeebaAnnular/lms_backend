ff2e45fa5173813e54ca8da96be7818689000913
DeebaAnnular <deebalakshmi.m@annulartechnologies>
2024-06-27 15:46:27 +0530

    db configuration changed

--- config/db.js
+++ config/db.js
@@ -2,12 +2,24 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+// const pool = mysql.createPool({
+//     host:process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password :process.env.DB_PASSWORD,
+//     database:process.env.DB_NAME
+// });
+
 const pool = mysql.createPool({
-    host:process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password :process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
+    host: 'lms-db.cjum8q0m8h5v.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'lmsdb2024',
+    database: 'lmsdb',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
+
+
 
 // Test the database connection
 pool.getConnection((err, connection) => {
