8de5e13855f8674920c7972af8a0beb3423a5fe6
DeebaAnnular <deebalakshmi.m@annulartechnologies>
2024-08-06 12:19:08 +0530

    weekly_status table name updated

--- models/weeklyStatus.js
+++ models/weeklyStatus.js
@@ -2,12 +2,12 @@ const db = require('../config/db');
 
 const WeeklyStatus = {
   create: (userData, callback) => {
-    const query = 'INSERT INTO Weekly_status (user_id, from_date, to_date) VALUES (?, ?, ?)';
+    const query = 'INSERT INTO weekly_status (user_id, from_date, to_date) VALUES (?, ?, ?)';
     db.query(query, [userData.user_id, userData.from_date, userData.to_date], callback);
   },
 
   update: (id, data, callback) => {
-    const query = 'UPDATE Weekly_status SET approved_status = ?, approved_by = ?, comment = ? WHERE week_id = ?';
+    const query = 'UPDATE weekly_status SET approved_status = ?, approved_by = ?, comment = ? WHERE week_id = ?';
     db.query(query, [data.approved_status, data.approved_by, data.comment, id], callback);
   },
 
@@ -15,7 +15,7 @@ const WeeklyStatus = {
     const query = `
       SELECT ws.*, 
              SUM(ds.total_duration) as total_week_duration
-      FROM Weekly_status ws
+      FROM weekly_status ws
       LEFT JOIN Daily_status ds ON ds.user_id = ws.user_id 
                                 AND ds.date BETWEEN ws.from_date AND ws.to_date
       WHERE ws.user_id = ? AND ws.from_date >= ? AND ws.to_date <= ?
