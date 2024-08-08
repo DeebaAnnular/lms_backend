38bacf5e9bba62d01bdd7c1066e4bee232ec6fc2
DeebaAnnular <deebalakshmi.m@annulartechnologies>
2024-08-08 15:26:42 +0530

    ssl added

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 const express = require("express");
 const authRoutes = require("./routes/authRoutes");
 const leaveRoutes = require("./routes/leaveManagementRoutes");
-const tasksRoutes = require ("./routes/taskRoutes");
+const tasksRoutes = require("./routes/taskRoutes");
 const holidayRoutes = require("./routes/holidayRoutes");
-const cors = require('cors')
+const cors = require("cors");
 
 require("dotenv").config();
 
 const app = express();
 
-app.use(express.json());
-app.use(cors())
-app.use("/api/auth", authRoutes);
-app.use("/api/leave", leaveRoutes);
-app.use("/api/task", tasksRoutes);
-app.use("/api/holiday",holidayRoutes);
+// /etc/letsencrypt/live/lms-api.annularprojects.com/cert.pem
+
+const sslOptions = {
+  key: fs.readFileSync(
+    "/etc/letsencrypt/live/lms-api.annularprojects.com/privkey.pem"
+  ),
+  cert: fs.readFileSync(
+    "/etc/letsencrypt/live/lms-api.annularprojects.com/fullchain.pem"
+  ),
+};
+
+const server = https.createServer(sslOptions, app);
+
+server.use(express.json());
+server.use(cors());
+server.use("/api/auth", authRoutes);
+server.use("/api/leave", leaveRoutes);
+server.use("/api/task", tasksRoutes);
+server.use("/api/holiday", holidayRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
